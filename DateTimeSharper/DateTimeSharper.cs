4c7617b924016ac7eed267e5379ae7fe86fa55ef
vndpal <undervinod@gmail.com>
2024-05-11 16:29:15 +0530

    initial commit

---
+++ DateTimeSharper/DateTimeSharper.cs
@@ -0,0 +1,20 @@
+﻿namespace DateTimeSharper
+{
+    public static class DateTimeSharper
+    {
+        public static DateTime getCurrentDateTime(TimeZone timeZone)
+        {
+            return DateTime.Now;
+        }
+    }
+
+    public enum TimeZone
+    {
+        Local,
+        Utc,
+        Ist,
+        Est,
+        Pst
+    }
+
+}
